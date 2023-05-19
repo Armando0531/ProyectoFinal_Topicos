14ebd20e448a84cba64e6bb3dfcac76bea39fa97
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-05-18 18:43:09 -0600

    Se creo el modelo para los usuario con sus get y set

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,4 +1,38 @@
 package modelo;
 
 public class Usuario {
+    private String nombre;
+    private String contrasena;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String contrasena) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                '}';
+    }
 }
