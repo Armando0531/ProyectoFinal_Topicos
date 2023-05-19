a0259e30325cf0e4a2bbd4cb35c774c25f394968
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-05-18 19:13:08 -0600

    Se agregaron los elementos al modelo donador

--- src/modelo/Donador.java
+++ src/modelo/Donador.java
@@ -1,4 +1,93 @@
 package modelo;
 
 public class Donador {
+    private int id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String categoria;
+    private String anioGraduacion;
+    private String metodoPago;
+
+    public Donador(){
+
+    }
+    public Donador(int id, String nombre, String direccion, String telefono, String categoria, String anioGraduacion, String metodoPago) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.categoria = categoria;
+        this.anioGraduacion = anioGraduacion;
+        this.metodoPago = metodoPago;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getAnioGraduacion() {
+        return anioGraduacion;
+    }
+
+    public void setAnioGraduacion(String anioGraduacion) {
+        this.anioGraduacion = anioGraduacion;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    @Override
+    public String toString() {
+        return "Donador{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", anioGraduacion=" + anioGraduacion +
+                ", metodoPago='" + metodoPago + '\'' +
+                '}';
+    }
 }
