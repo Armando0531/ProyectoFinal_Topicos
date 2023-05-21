fad0fd7093d295e099bfe9556d7bf29694f35c3e
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-05-20 19:36:12 -0600

    Se creo la conexio a la base de datos

--- src/conexionBD/ConexionBD.java
+++ src/conexionBD/ConexionBD.java
@@ -1,4 +1,45 @@
 package conexionBD;
 
+import java.sql.*;
+
 public class ConexionBD {
+    private static Connection conexion;
+    private static PreparedStatement pstm;
+    private static ResultSet rs;
+
+    static String URL;
+
+    public ConexionBD() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            URL = "jdbc:mysql://localhost:3306/Universidad";
+
+            conexion = DriverManager.getConnection(URL, "root", "jael0531");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error de DRIVER");
+        } catch (SQLException e) {
+            System.out.println("Error de conexion en MySQL");
+        }
+    }
+    public static Connection getConexion(){
+
+        if (conexion == null){
+            new ConexionBD();
+        }
+
+        return conexion;
+    }
+
+
+    static void cerrarConexion() {
+        try {
+            pstm.close();
+            conexion.close();
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexion");
+            e.printStackTrace();
+        }
+    }
 }
