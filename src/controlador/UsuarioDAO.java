31ba513514e300ebdf0278e1f5c15eec70f0e982
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-05-22 18:31:14 -0600

    Se creo un metodo en el controlador Usuario para buscar elementos en la base de datos

--- src/controlador/UsuarioDAO.java
+++ src/controlador/UsuarioDAO.java
@@ -1,4 +1,33 @@
 package controlador;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import conexionBD.ConexionBD;
+import modelo.Usuario;
+
 public class UsuarioDAO {
+
+    public ArrayList<Usuario> buscar(String nombre, String contrasena) {
+
+        ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+        String sql = "SELECT * FROM Usuarios WHERE Nombre = '" + nombre + "' AND Contrasena = '" + contrasena + "';";
+
+        ResultSet rs = ConexionBD.ConsultarRegistro(sql);
+
+        try {
+            if (rs.next()) {
+                do {
+                    listaUsuarios.add(new Usuario(rs.getString(1), rs.getString(2)));
+                } while (rs.next());
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return listaUsuarios;
+    }
 }
