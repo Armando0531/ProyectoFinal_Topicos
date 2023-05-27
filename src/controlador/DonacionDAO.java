51c40e49aef66a9ef9c2b9c191fc567ef7b98829
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-05-26 19:23:12 -0600

    Se creo el metodo insertar registro en el controlador de donacion

--- src/controlador/DonacionDAO.java
+++ src/controlador/DonacionDAO.java
@@ -1,4 +1,23 @@
 package controlador;
 
-public class DonacionDAO {
+import conexionBD.ConexionBD;
+import modelo.Donacion;
+
+
+public class DonacionDAO  {
+    ConexionBD conexion;
+
+    public DonacionDAO() {
+        conexion = new ConexionBD();
+    }
+
+    public boolean insertarRegistro(Donacion a) {
+        boolean resultado=false;
+
+        resultado=ConexionBD.AgregarDonacion(a);
+
+        return resultado;
+    }
+
+
 }
