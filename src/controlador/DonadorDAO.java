374886415246b300c354450986aae09da582e64e
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-05-30 15:38:27 -0600

    Se creo un metodo para insertar donador en el controlador donador

--- src/controlador/DonadorDAO.java
+++ src/controlador/DonadorDAO.java
@@ -1,4 +1,27 @@
 package controlador;
 
-public class DonadorDAO {
+import conexionBD.ConexionBD;
+import modelo.Donador;
+import vista.VentanaEliminarDonador;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DonadorDAO implements Runnable {
+    ConexionBD conexion;
+    private int filtro;
+
+    public DonadorDAO() {
+        conexion = new ConexionBD();
+    }
+
+    public boolean insertarDonador(Donador a) {
+        boolean resultado=false;
+
+        resultado=ConexionBD.AgregarRegistro(a);
+
+        return resultado;
+    }
+
+
 }
