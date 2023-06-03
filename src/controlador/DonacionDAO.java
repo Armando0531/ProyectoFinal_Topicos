bab4e1ffc07b0300e0c5bfcf8695b513ae35d1da
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-06-02 21:43:28 -0600

    Se agregaron metodos para bsucar y eliminar donacion en el controlador donacion

--- src/controlador/DonacionDAO.java
+++ src/controlador/DonacionDAO.java
@@ -2,10 +2,14 @@ package controlador;
 
 import conexionBD.ConexionBD;
 import modelo.Donacion;
+import vista.VentanaEliminarDonacion;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class DonacionDAO  {
+public class DonacionDAO implements Runnable {
     ConexionBD conexion;
+    private int filtro;
 
     public DonacionDAO() {
         conexion = new ConexionBD();
@@ -19,5 +23,63 @@ public class DonacionDAO  {
         return resultado;
     }
 
+    public boolean modificarDonadocion(Donacion a) {
+        boolean resultado = false;
+        resultado=ConexionBD.ActualizarRegistroDonacion(a);
+
+        return resultado;
+    }
+
+    public Donacion buscar(int filtro){
+        Donacion d1=new Donacion();
+        String sql="SELECT * FROM Donativo WHERE ID ="+filtro+";";
+
+        ResultSet rs=ConexionBD.ConsultarRegistro(sql);
+
+        try {
+
+            if(rs.next()) {
+                try {
+                    d1.setId(rs.getInt(1));
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                d1.setDonanteId(rs.getInt(2));
+                d1.setEventoId(rs.getInt(3));
+                d1.setCantidadGarantizada(rs.getDouble(4));
+                d1.setCantidadEnviada(rs.getDouble(5));
+                d1.setFechaGarantia(rs.getString(6));
+                d1.setNumeroPagos(rs.getInt(7));
+                d1.setTarjetaCredito(rs.getString(8));
+                d1.setCorporacionEmisora(rs.getString(9));
+                d1.setDireccionCorporacion(rs.getString(10));
+                d1.setNombreConyuge(rs.getString(11));
+            }else {
+                VentanaEliminarDonacion.bandera=0;
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }//0==no 1==si
+        VentanaEliminarDonacion.bandera=1;
+        return d1;
+    }
+    public boolean eliminarDonacion(String idEliminar) {
+
+        String instruccion="DELETE FROM Donativo WHERE ID = '"+idEliminar+"';";
+        boolean resultado = false;
+        resultado = ConexionBD.EliminarRegistro(instruccion);
+
+        return resultado;
+    }
+
+    @Override
+    public void run() {
+        buscar(this.filtro);
+
+    }
+    public void setFiltro(int filtro) {
+        this.filtro = filtro;
+    }
 
 }
